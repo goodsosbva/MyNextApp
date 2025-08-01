33bc1267fc85d9d82829900aae442d8a2b674b26
khs <hs.kwan@okestro.com>
2025-08-01 22:47:21 +0900

    feat: feed layout init

---
+++ my-next-app/app/feed/layout.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+interface FeedLayoutProps {
+    children: React.ReactNode;
+}
+
+export default function FeedLayout(props: FeedLayoutProps) {
+    const { children } = props;
+    const [status, setStatus] = useState('mounting...');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setStatus('mounted');
+        }, 1000);
+    }, []);
+
+    return (
+        <div className="box layout">
+            <h4>{`FeedLayout: ${status}`}</h4>
+            <div style={{padding: 8}}>{children}</div>
+        </div>
+    );
+}
